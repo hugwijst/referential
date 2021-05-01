5a0a894e4b299425fd2976c4b3a7c481b3d6182a
Hugo van der Wijst <hugo@wij.st>
2021-05-01 15:07:22 -0700

    Cleanup, mark crate as `no_std`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,136 +1,152 @@
-use std::ops::Deref;
-use std::pin::Pin;
+#![cfg_attr(not(test), no_std)]
+
+// #![feature(trace_macros)]
 
 trait Referencer<'a, P: ?Sized> {
     fn from_data(data: &'a P) -> Self;
 }
 
-macro_rules! referential(
-    {
+#[macro_export]
+macro_rules! referential {
+    (
         struct $name:ident + $own_lifetime:lifetime ($ref:ident);
-    } => {
-        referential!{ struct $name + $own_lifetime ($ref<>); }
+    ) => {
+        referential!{ struct $name< > + $own_lifetime ($ref<>); }
     };
-    {
-        struct $name:ident + $own_lifetime:lifetime ($ref:ident<$($ref_lifetimes:lifetime),*>);
-    } => {
-        struct $name<P>(Pin<P>, $ref<'static>)
+    (
+
+        $vis:vis struct $name:ident<$($own_gen_lifetime:lifetime $(: $own_first_lifetime_bound:lifetime $(+ $own_other_lifetime_bound:lifetime)*)?),*> + $own_lifetime:lifetime ($ref:ident<$($ref_lifetimes:lifetime),*>);
+    ) => {
+        // The order of elements here is critical to ensure safety: the
+        // drop order is the same as declared here. First dropping the referenced
+        // data is obviously unsafe, dropping the references first should be safe.
+        $vis struct $name<$($own_gen_lifetime $(: $own_first_lifetime_bound $(+ $own_other_lifetime_bound)*)?),* P>($ref<'static>, ::std::pin::Pin<P>)
         where
-            P: Deref,
-            <P as Deref>::Target: Unpin;
+            P: ::core::ops::Deref,
+            <P as ::core::ops::Deref>::Target: Unpin;
 
         impl<P> $name<P>
         where
-            P: Deref,
-            <P as Deref>::Target: Unpin,
-            for<'a> $ref<'a>: Referencer<'a, <P as Deref>::Target>,
+            P: ::core::ops::Deref,
+            <P as ::core::ops::Deref>::Target: Unpin,
+            for<'a> $ref<'a>: $crate::Referencer<'a, <P as ::core::ops::Deref>::Target>,
         {
             #![allow(dead_code)]
 
             pub fn new(pinnable: P) -> Self {
-                let pinned_data = Pin::new(pinnable);
+                let pinned_data = ::core::pin::Pin::new(pinnable);
                 let references_local = $ref::from_data(pinned_data.as_ref().get_ref());
                 let references_static = unsafe {
-                    std::mem::transmute::<$ref<'_>, $ref<'static>>(references_local)
+                    ::core::mem::transmute::<$ref<'_>, $ref<'static>>(references_local)
                 };
 
-                Self(pinned_data, references_static)
+                Self(references_static, pinned_data)
             }
 
-            pub fn pinned<'a>(&'a self) -> &'a <P as Deref>::Target {
-                self.0.as_ref().get_ref()
+            pub fn pinned<'a>(&'a self) -> &'a <P as ::core::ops::Deref>::Target {
+                self.1.as_ref().get_ref()
             }
 
             pub fn referenced<'a>(&'a self) -> &'a $ref<'a> {
-                &self.1
+                &self.0
             }
         }
-    }
-);
-
-struct LargeDataOwned<const N: usize> {
-    large_array: [u8; N],
-    large_vec: Vec<u8>,
+    };
 }
 
-struct LargeDataRefs<'a> {
-    last_array_element: &'a u8,
-    last_vec_element: &'a u8,
-}
+// trace_macros!(true);
 
-impl<'a, const N: usize> Referencer<'a, LargeDataOwned<N>> for LargeDataRefs<'a> {
-    fn from_data(data: &'a LargeDataOwned<N>) -> Self {
-        Self {
-            last_array_element: &data.large_array[data.large_array.len() - 1],
-            last_vec_element: &data.large_vec[data.large_vec.len() - 1],
-        }
-    }
-}
-
-//// vvvv TO BE GENERATED vvvv ////
-referential! {
-    struct NoReference + 'a (LargeDataRefs);
-}
-
-
-referential! {
-    struct LargeData2 + 'a (LargeDataRefs<'a>);
-}
-
-struct LargeData<P>(Pin<P>, LargeDataRefs<'static>)
-where
-    P: Deref,
-    <P as Deref>::Target: Unpin;
-
-impl<P> LargeData<P>
-where
-    P: Deref,
-    <P as Deref>::Target: Unpin,
-    for<'a> LargeDataRefs<'a>: Referencer<'a, <P as Deref>::Target>,
-{
-    fn new(pinnable: P) -> Self {
-        let pinned_data = Pin::new(pinnable);
-        let references_local = LargeDataRefs::from_data(pinned_data.as_ref().get_ref());
-        let references_static = unsafe {
-            std::mem::transmute::<LargeDataRefs<'_>, LargeDataRefs<'static>>(references_local)
-        };
-
-        Self(pinned_data, references_static)
-    }
-
-    fn pinned<'a>(&'a self) -> &'a <P as Deref>::Target {
-        self.0.as_ref().get_ref()
-    }
-
-    fn referenced<'a>(&'a self) -> &'a LargeDataRefs<'a> {
-        &self.1
-    }
-}
+// referential! {
+//     struct WithUnusedOwnLifetime<'a> + 'b (LargeDataRefs<'b>);
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use std::rc::Rc;
+    struct OwnedVec {
+        vec: Vec<u8>,
+    }
+
+    impl OwnedVec {
+        fn new(n: usize) -> OwnedVec {
+            OwnedVec {
+                vec: (0..n as u8).collect(),
+            }
+        }
+    }
+
+    struct OwnedConstGeneric<const N: usize> {
+        array: [u8; N],
+    }
+
+    impl<const N: usize> Default for OwnedConstGeneric<N> {
+        fn default() -> OwnedConstGeneric<N> {
+            use core::convert::TryFrom;
+            OwnedConstGeneric {
+                array: <[u8; N]>::try_from((0..N as u8).collect::<Vec<_>>().as_ref()).unwrap(),
+            }
+        }
+    }
+
+    pub struct Refs<'a> {
+        last_element: &'a u8,
+    }
+
+    impl<'a> Referencer<'a, OwnedVec> for Refs<'a> {
+        fn from_data(data: &'a OwnedVec) -> Self {
+            Self {
+                last_element: &data.vec[data.vec.len() - 1],
+            }
+        }
+    }
+
+    impl<'a, const N: usize> Referencer<'a, OwnedConstGeneric<N>> for Refs<'a> {
+        fn from_data(data: &'a OwnedConstGeneric<N>) -> Self {
+            Self {
+                last_element: &data.array[N - 1],
+            }
+        }
+    }
 
     #[test]
-    fn it_works() {
-        let data = LargeDataOwned {
-            large_array: [1, 2, 3, 4, 5, 6, 7, 8],
-            large_vec: (5..=10).collect(),
-        };
-        let large_data = LargeData::new(Box::new(data));
+    fn no_generics() {
+        referential! {
+            struct NoGenerics + 'a (Refs);
+        }
 
-        assert_eq!(*large_data.referenced().last_array_element, 8);
-        assert_eq!(*large_data.referenced().last_vec_element, 10);
+        let referential = NoGenerics::new(Box::new(OwnedVec::new(5)));
+        assert_eq!(*referential.referenced().last_element, 4);
+        assert!(core::ptr::eq(
+            referential.referenced().last_element,
+            &referential.pinned().vec[referential.pinned().vec.len() - 1]
+        ));
+    }
 
-        let data2 = LargeDataOwned {
-            large_array: [1, 2],
-            large_vec: vec![1, 2],
-        };
-        let large_data2 = LargeData2::new(Rc::new(data2));
+    #[test]
+    fn empty_generics() {
+        referential! {
+            struct EmptyGenerics< > + 'a (Refs<'a>);
+        }
 
-        assert_eq!(*large_data2.referenced().last_array_element, 2);
-        assert_eq!(*large_data2.referenced().last_vec_element, 2);
+        let referential = EmptyGenerics::new(Box::new(OwnedVec::new(5)));
+        assert_eq!(*referential.referenced().last_element, 4);
+        assert!(core::ptr::eq(
+            referential.referenced().last_element,
+            &referential.pinned().vec[referential.pinned().vec.len() - 1]
+        ));
+    }
+
+    #[test]
+    fn rc_pinned() {
+        use std::rc::Rc;
+        referential! {
+            struct Simple<> + 'a (Refs<'a>);
+        }
+
+        let data = OwnedVec::new(2);
+        let simple = Simple::new(Rc::new(data));
+
+        assert_eq!(*simple.referenced().last_element, 1);
     }
 }
